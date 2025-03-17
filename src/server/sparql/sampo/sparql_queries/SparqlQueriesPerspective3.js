1f52149e79d54a021ae0d028a75401b697a8c0b1
TroubleDOG0100 <bingogrislis@gmail.com>
2025-03-17 21:09:38 +0200

    Added universities category, modified GeneralQuery for facetValuesQuery since BIND(Coalesce...) doesn't work w/ Sparql access point.

--- src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective3.js
+++ src/server/sparql/sampo/sparql_queries/SparqlQueriesPerspective3.js
@@ -1,92 +1,30 @@
-const perspectiveID = 'perspective3'
+const perspectiveID = 'universities'
 
-export const eventProperties = `
+export const workProperties = `
     {
-      ?id a ?type__id .
-      ?type__id skos:prefLabel|rdfs:label ?type__prefLabel .
-      BIND(?type__id as ?prefLabel__id)
-      BIND(?type__prefLabel as ?prefLabel__prefLabel)
-      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?type__dataProviderUrl)
-      BIND(?id as ?uri__id)
-      BIND(?id as ?uri__dataProviderUrl)
-      BIND(?id as ?uri__prefLabel)
+      ?id   rdfs:label ?prefLabel__id .
+      BIND(?prefLabel__id AS ?prefLabel__prefLabel)
+      BIND(CONCAT("/${perspectiveID}/page/", REPLACE(STR(?id), "^.*\\\\/(.+)", "$1")) AS ?prefLabel__dataProviderUrl)
+      BIND(?final_id as ?uri__id)
+      BIND(?final_id as ?uri__dataProviderUrl)
+      BIND(?final_id as ?uri__prefLabel)
+      FILTER(LANG(?prefLabel__prefLabel) = 'en')
+      
     }
-    UNION
-    {
-      ?id crm:P7_took_place_at ?place__id .
-      ?place__id skos:prefLabel ?place__prefLabel .
-      BIND(CONCAT("/places/page/", REPLACE(STR(?place__id), "^.*\\\\/(.+)", "$1")) AS ?place__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id crm:P4_has_time-span ?eventTimespan__id .
-      ?eventTimespan__id skos:prefLabel ?eventTimespan__prefLabel .
-      OPTIONAL { ?eventTimespan__id crm:P82a_begin_of_the_begin ?eventTimespan__start }
-      OPTIONAL { ?eventTimespan__id crm:P82b_end_of_the_end ?eventTimespan__end }
-    }
-    UNION
-    {
-      ?id crm:P30_transferred_custody_of ?manuscript__id .
-      ?manuscript__id skos:prefLabel ?manuscript__prefLabel .
-      OPTIONAL {
-        ?manuscript__id a frbroo:F4_Manifestation_Singleton .
-        BIND(CONCAT("/manuscripts/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
-      }
-      OPTIONAL {
-        ?manuscript__id a crm:E78_Collection  .
-        BIND(CONCAT("/collections/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
-      }
-      OPTIONAL {
-        ?id crm:P28_custody_surrendered_by ?surrender__id .
-        ?surrender__id skos:prefLabel ?surrender__prefLabel .
-        BIND(CONCAT("/actors/page/", REPLACE(STR(?surrender__id), "^.*\\\\/(.+)", "$1")) AS ?surrender__dataProviderUrl)
-      }
-      OPTIONAL {
-        ?id crm:P29_custody_received_by ?receiver__id .
-        ?receiver__id skos:prefLabel ?receiver__prefLabel .
-        BIND(CONCAT("/actors/page/", REPLACE(STR(?receiver__id), "^.*\\\\/(.+)", "$1")) AS ?receiver__dataProviderUrl)
-      }
-    }
-    UNION
-    {
-      ?id mmm-schema:observed_manuscript ?manuscript__id .
-      ?manuscript__id skos:prefLabel ?manuscript__prefLabel .
-      OPTIONAL {
-        ?id mmm-schema:ownership_attributed_to ?observedOwner__id .
-        ?observedOwner__id skos:prefLabel ?observedOwner__prefLabel .
-        BIND(CONCAT("/actors/page/", REPLACE(STR(?observedOwner__id), "^.*\\\\/(.+)", "$1")) AS ?observedOwner__dataProviderUrl)
-      }
-      OPTIONAL {
-        ?manuscript__id a frbroo:F4_Manifestation_Singleton .
-        BIND(CONCAT("/manuscripts/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
-      }
-      OPTIONAL {
-        ?manuscript__id a crm:E78_Collection  .
-        BIND(CONCAT("/collections/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
-      }
-    }
-    UNION
-    {
-      ?id crm:P108_has_produced ?manuscript__id .
-      ?manuscript__id skos:prefLabel ?manuscript__prefLabel .
-      BIND(CONCAT("/manuscripts/page/", REPLACE(STR(?manuscript__id), "^.*\\\\/(.+)", "$1")) AS ?manuscript__dataProviderUrl)
-    }
-    UNION
-    {
-      ?id dct:source ?source__id .
-      ?source__id skos:prefLabel ?source__prefLabel .
-      ?source__id mmm-schema:data_provider_url ?source__dataProviderUrl .
-    }
-    UNION
-    {
-      ?id mmm-schema:data_provider_url ?source__id .
-      BIND(?source__id as ?source__dataProviderUrl)
-      BIND(?source__id as ?source__prefLabel)
-    }
-    UNION
-    {
-      ?id crm:P3_has_note ?note .
-    }
+    OPTIONAL {?id dbo:country ?country__id .
+                          ?country__id rdfs:label ?countryLabel ;
+                          # Retrieve the url to the dbo:Country in publicly available data provider.
+                                          owl:sameAs ?country__dataProviderUrl .
+                          BIND(STR(?countryLabel) AS ?country__prefLabel)
+                          FILTER(LANG(?countryLabel) = 'en')
+    }                          
+    OPTIONAL {?id dbo:city ?city__id .
+                      ?city__id rdfs:label ?cityLabel ;
+                      # Retrieve the url to the dbo:city publicly available data provider.
+                                      owl:sameAs ?city__dataProviderUrl .
+                      BIND(STR(?cityLabel) AS ?city__prefLabel)
+                      FILTER(LANG(?cityLabel) = 'en')              
+    }        
 `
 
 export const eventPlacesQuery = `
